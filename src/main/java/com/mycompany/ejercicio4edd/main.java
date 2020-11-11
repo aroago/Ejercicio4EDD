a6b1bed64eab780b9f1dd506d8fb8e472e7669ba
AroaGO <74042599+aroago@users.noreply.github.com>
2020-11-11 13:30:20 +0100

    Initial commit

---
+++ src/main/java/com/mycompany/ejercicio4edd/main.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.ejercicio4edd;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author aroa
+ */
+public class main {
+
+    /**
+     * @param args this program contains conditionals.
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner teclado=new Scanner(System.in);
+        int n;
+		
+		System.out.println("Introduce un numero entero: ");
+		n=teclado.nextInt();
+		
+		if(n<0){
+                    System.out.println("NEGATIVO");
+                }else{
+                    System.out.println("POSITIVO");
+                }
+    }
+    
+}
